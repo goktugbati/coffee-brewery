42eadff6eb5b210001d0f52a763d78647be84384
goktug <goktug.bati@gmail.com>
2025-02-20 00:06:33 +0300

    Initial Commit

---
+++ coffee-microservice/README.md
@@ -0,0 +1,3 @@
+# Coffee Brewery Microservice
+
+Spring Boot Microservice for Coffee Brewery
---
+++ coffee-microservice/src/test/java/com/dataflow/coffeemicroservice/CoffeeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataflow.coffeemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
